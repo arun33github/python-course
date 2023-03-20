d754a88d5ce9c3e0a9d1c306ac7b973b695bd56a
arun <starunselvam@gmail.com>
2023-03-19 20:14:54 -0700

    area of circle prog

---
+++ circle.py
@@ -0,0 +1,10 @@
+def  area_circle(r):
+     radius=3.14*r*r
+     print(radius)
+
+
+
+
+def   volume_sphere(r):
+      v=4/3*3.14*r*r*r
+      print(v)
---
+++ run.py
@@ -0,0 +1,11 @@
+from circle import area_circle
+
+radius=int(input("enter a value: "))
+area_circle(radius)
+
+
+
+from circle import volume_sphere
+
+v=int(input("enter a value: "))
+volume_sphere(v)
