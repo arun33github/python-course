5fc61112cabef4108a0ba1183d39b0212de07750
Bala <starunselvam@gmail.com>
2023-04-02 20:32:39 -0700

    patter

---
+++ pattern.py
@@ -0,0 +1,47 @@
+# *
+# **
+# ***
+# ****
+# *****
+# print() 
+
+# n=int(input("enter your value :"))
+# i=1
+# while i<=n:
+#       j=0
+#       while j<i:
+#          print("*",end="")
+#          j+=1
+#       print()
+#       i+=1
+
+
+
+# *****
+# **** 
+# ***
+# **   
+# *  
+# print()
+ 
+# n=int(input("enter the value :"))
+# i=1
+# while i>=n:
+#     j=0
+#     while j>i:
+#         print("*",end="")
+#         j-=1
+#         print()
+#     i-=1
+i=1
+while i<=5:
+    j=5
+
+    while j>=i:
+        print("*",end="")
+        j-=1
+    print()
+    i+=1
+
+#i=1 j=1  s=4
+#i=2 j=1,2  s=3
